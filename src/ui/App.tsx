98ee3b5a789f02af6f5765698bd7ae6808acd5f4
Ryo Nakae <ryo.n.1129@gmail.com>
2025-03-14 13:55:27 +0900

    Update build script to use input.css and add new input.css for Tailwind CSS theming

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -4,7 +4,7 @@ import { type JSX, h } from 'preact'
 export default function App() {
   return (
     <div id="wrapper">
-      <h1 className="text-text-primary font-bold text-2xl">
+      <h1 className="font-bold text-2xl text-text-primary">
         sync-variables-with-notion
       </h1>
     </div>
