ab08656515776633aa1d9a426fe950cbddc9f08c
Ryo Nakae <ryo.n.1129@gmail.com>
2025-03-19 16:40:44 +0900

    Enhance Collection component to manage Notion settings; add input fields for Database ID, Integration token, and Key property name with state management using useSettings hook

--- src/ui/components/tabs/Collection.tsx
+++ src/ui/components/tabs/Collection.tsx
@@ -1,11 +1,23 @@
 /** @jsx h */
-import { h } from 'preact'
+import { type JSX, h } from 'preact'
 
 import { useMount, useUnmount } from 'react-use'
 
 import TabItem from '@/ui/components/TabItem'
+import useSettings from '@/ui/hooks/useSettings'
+import { Stack, Textbox } from '@create-figma-plugin/ui'
 
 export default function Collection() {
+  const { settings, tmpSettings, updateSettings } = useSettings()
+
+  function handleInput(key: keyof Settings) {
+    return (event: JSX.TargetedEvent<HTMLInputElement>) => {
+      updateSettings({
+        [key]: event.currentTarget.value,
+      })
+    }
+  }
+
   useMount(() => {
     console.log('Collection: mounted')
     console.log(process.env.PROXY_URL)
@@ -17,7 +29,34 @@ export default function Collection() {
 
   return (
     <TabItem>
-      <h1>Collection</h1>
+      <Stack space="small">
+        <div className="flex flex-col gap-1">
+          <div>Database ID</div>
+          <Textbox
+            onInput={handleInput('notionDatabaseId')}
+            value={settings.notionDatabaseId}
+            disabled={tmpSettings.loading}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div>Integration token</div>
+          <Textbox
+            onInput={handleInput('notionIntegrationToken')}
+            value={settings.notionIntegrationToken}
+            disabled={tmpSettings.loading}
+          />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <div>Key property name</div>
+          <Textbox
+            onInput={handleInput('notionKeyPropertyName')}
+            value={settings.notionKeyPropertyName}
+            disabled={tmpSettings.loading}
+          />
+        </div>
+      </Stack>
     </TabItem>
   )
 }
